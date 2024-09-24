0024f43b68c56099a51d41dd6ee7d0e232e09e59
christinesn <51234471+christinesn@users.noreply.github.com>
2024-09-24 14:53:01 -0400

    Style footer

--- src/Footer.js
+++ src/Footer.js
@@ -3,37 +3,39 @@ import './Footer.css'
 export default function Footer () {
     return (
         <footer>
-            <img
-                src="/images/restaurantfood.jpg"
-                alt=""
-            />
-            <article>
-                <h6>Navigation</h6>
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Menu</a></li>
-                    <li><a href="#">Reservations</a></li>
-                    <li><a href="#">Order Online</a></li>
-                    <li><a href="#">Login</a></li>
-                </ul>
-            </article>
-            <article>
-                <h6>Contact</h6>
-                <ul>
-                    <li><a href="#">Address</a></li>
-                    <li><a href="#">Phone Number</a></li>
-                    <li><a href="#">Email</a></li>
-                </ul>
-            </article>
-            <article>
-                <h6>Social Media Links</h6>
-                <ul>
-                    <li><a href="#">Address</a></li>
-                    <li><a href="#">Phone Number</a></li>
-                    <li><a href="#">Email</a></li>
-                </ul>
-            </article>
+            <div className="container footer-content">
+                <img
+                    src="/images/restaurantfood.jpg"
+                    alt=""
+                />
+                <article>
+                    <h2>Navigation</h2>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Menu</a></li>
+                        <li><a href="#">Reservations</a></li>
+                        <li><a href="#">Order Online</a></li>
+                        <li><a href="#">Login</a></li>
+                    </ul>
+                </article>
+                <article>
+                    <h2>Contact</h2>
+                    <ul>
+                        <li><a href="#">Address</a></li>
+                        <li><a href="#">Phone</a></li>
+                        <li><a href="#">Email</a></li>
+                    </ul>
+                </article>
+                <article>
+                    <h2>Social Media</h2>
+                    <ul>
+                        <li><a href="#">Facebook</a></li>
+                        <li><a href="#">Twitter</a></li>
+                        <li><a href="#">Instagram</a></li>
+                    </ul>
+                </article>
+            </div>
         </footer>
     )
 }
