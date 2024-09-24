a73fcb750527d9b0f44b1f40ec5ccad0cf955b6f
christinesn <51234471+christinesn@users.noreply.github.com>
2024-09-24 14:01:53 -0400

    Style testimonials section

--- src/Testimonials.js
+++ src/Testimonials.js
@@ -1,32 +1,47 @@
 import './Testimonials.css'
 
 const testimonials = [{
-    rating: "Rating",
-    name: "Name1",
-    text: "Review text"
+    rating: "Lorem ipsum",
+    name: "Kaylee",
+    text: "Etiam ornare ultrices sapien ac venenatis.",
+    image: "/images/kaylee.jpg"
 }, {
-    rating: "Rating",
-    name: "Name2",
-    text: "Review text"
+    rating: "Lorem ipsum",
+    name: "Wilfred",
+    text: "Etiam ornare ultrices sapien ac venenatis.",
+    image: "/images/wilfred.jpg"
 }, {
-    rating: "Rating",
-    name: "Name3",
-    text: "Review text"
+    rating: "Lorem ipsum",
+    name: "Nadia",
+    text: "Etiam ornare ultrices sapien ac venenatis.",
+    image: "/images/nadia.jpg"
+}, {
+    rating: "Lorem ipsum",
+    name: "Mary",
+    text: "Etiam ornare ultrices sapien ac venenatis.",
+    image: "/images/mary.jpg"
 }]
 
 export default function Testimonials () {
     return (
         <section id="testimonials">
-            <h2>Testimonials</h2>
-            <section>
-                {testimonials.map((review) => (
-                    <article key={review.name}>
-                        <h3>{review.rating}</h3>
-                        <div>{review.name}</div>
-                        <div>{review.text}</div>
-                    </article>
-                ))}
-            </section>
+            <div className="container">
+                <h2>Testimonials</h2>
+                <div className="reviews">
+                    {testimonials.map((review) => (
+                        <article className="review" key={review.name}>
+                            <h3>{review.rating}</h3>
+                            <div className="reviewer">
+                                <img src={review.image} alt={review.name} />
+                                <span className="name">{review.name}</span>
+                            </div>
+                            <div className="review-text">
+                                {review.text}
+                            </div>
+                        </article>
+                    ))}
+                </div>
+            </div>
         </section>
     )
 }
