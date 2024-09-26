57142f189ab9cc20d852a9764399a788e60e75e3
christinesn <51234471+christinesn@users.noreply.github.com>
2024-09-26 15:10:32 -0400

    Update updateTimes and initializeTimes unit tests

--- src/Main.test.js
+++ src/Main.test.js
@@ -1,13 +1,13 @@
 import { updateTimes, initializeTimes } from './Main'
 
 describe('Main', () => {
-    test('initializeTimes returns the expected value', () => {
+    test('initializeTimes returns a non-empty array', () => {
         const times = initializeTimes()
-        expect(times[0]).toBe('17:00')
+        expect(times.length).not.toBe(0)
     })
 
-    test('updateTimes returns the expected value', () => {
-        const times = updateTimes()
-        expect(times[0]).toBe('17:00')
+    test('updateTimes returns a non-empty array', () => {
+        const times = updateTimes(null, { date: new Date() })
+        expect(times.length).not.toBe(0)
     })
 })
